6ef01b01ac5b2d7a52b6d5ac8f6b705db5d8128e
Dannydorestant <98496978+Dannydorestant@users.noreply.github.com>
2022-02-11 14:24:56 -0500

    Get user input for guesses Manage guesses

---
+++ guesses.py
@@ -0,0 +1,14 @@
+# List of guesses
+guesses = []
+
+# Ask you user to guess a letter and store it in guesses list
+userinput: bool = True
+while userinput:
+    letter = input("Guess A Letter ")
+    if not letter in guesses:
+        confirmed = input("Are you Sure? type 'YES' to Confirm, 'NO' to select a different letter ").upper()
+        if confirmed == 'YES':
+            userinput: bool = False
+            guesses.append(letter)
+
+print(guesses)
