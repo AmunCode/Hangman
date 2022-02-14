5584725444eeb0d9d0d5ec07661991ed308983f9
Evelyn Brooks <98491379+Evelynb92@users.noreply.github.com>
2022-02-14 02:20:07 +0000

    Added pt 3.

--- main.py
+++ main.py
@@ -69,3 +69,36 @@ while userinput:
             print('You can only guess ONE letter at a time')
     else:
         print('Invalid Character, You can only guess a Letter')
+
+
+#Thot 
+guess = input('Please guess a letter:').upper()
+if len(guess) == 1 and guess.isalpha():
+  if guess in guessed_letters:
+    print('You already guessed this letter'), guess)
+elif guess not in word:
+  print(guess, 'is not on word.')
+  tries -= 1
+  guessed_letters.append(guess)
+else:
+  print('Good Job!,',guess,'is in the word!')
+  guessed_ letters.append(guess)
+  word_list = list(word_completion)
+indices =[i for i in enumerate(word)]if letter == guess]
+for index in indices:
+  word_list[index] = guess
+  word_completion = "".join(word_list)
+  if "_" not in word_ccompletion:
+    guessed = True
+elif len(guess) == len(word) and guess.isalpha():
+ if guess in guessed_words:
+   print'(You already guessed the word!', guess)
+ elif guess != word:
+   print(guess, 'is not the word!')
+   tries -= 1
+   guessed_words.append(guess)
+ else:
+   guessed = True
+   word_completion = word
+else:
+  print('Not a valid guess!')
